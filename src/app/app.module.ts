5760ce742e3ac4de3e4000d64efcbcce81f89740
Karmit Personal <karmit199@gmail.com>
2020-12-23 16:22:19 -0500

    Added a Drop Down Menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {StartupPageComponent} from './startup-page/startup-page.component';
 import {HexagonHoverComponent} from './hexagon-hover/hexagon-hover.component';
 import {NeomorphComponent} from './neomorph/neomorph.component';
+import {DropdownMenuComponent} from './dropdown-menu/dropdown-menu.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import {NeomorphComponent} from './neomorph/neomorph.component';
     ToastComponent,
     StartupPageComponent,
     HexagonHoverComponent,
-    NeomorphComponent
+    NeomorphComponent,
+    DropdownMenuComponent
   ],
   imports: [
     BrowserModule,
