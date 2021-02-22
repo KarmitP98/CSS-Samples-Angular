aa97fa629034288cba5daabed4d435110320a021
Karmit Personal <karmit199@gmail.com>
2021-02-22 16:09:15 -0500

    Added Panel Component.

---
+++ src/app/custom-comps/custom-comps.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-comps',
+  templateUrl: './custom-comps.component.html',
+  styleUrls: ['./custom-comps.component.scss']
+})
+export class CustomCompsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
