dd2dd5ac437aaed164ca2d755028db064d34a65f
Karmit Personal <karmit199@gmail.com>
2021-02-23 16:45:06 -0500

    Added a Temperature GUI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,18 +12,18 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {StartupPageComponent} from './startup-page/startup-page.component';
 import {HexagonHoverComponent} from './hexagon-hover/hexagon-hover.component';
 import {NeomorphComponent} from './neomorph/neomorph.component';
+
+import {CustomCompsComponent} from './custom-comps/custom-comps.component';
+import {PanelComponent} from './panel/panel.component';
+import {SlideComponent} from './slide/slide.component';
+import {TempComponent} from './temp/temp.component';
+
+import {GlassMorphismComponent} from './glass-morphism/glass-morphism.component';
+import {ComponentsComponent} from './components/components.component';
+import {SideMenuComponent} from './side-menu/side-menu.component';
+import {VideoComponent} from './video/video.component';
+import {WebcamModule} from 'ngx-webcam';
 import {DropdownMenuComponent} from './dropdown-menu/dropdown-menu.component';
-<<<<<<< HEAD
-import { CustomCompsComponent } from './custom-comps/custom-comps.component';
-import { PanelComponent } from './panel/panel.component';
-import { SlideComponent } from './slide/slide.component';
-=======
-import { GlassMorphismComponent } from './glass-morphism/glass-morphism.component';
-import { ComponentsComponent } from './components/components.component';
-import { SideMenuComponent } from './side-menu/side-menu.component';
-import { VideoComponent } from './video/video.component';
-import {WebcamModule} from "ngx-webcam";
->>>>>>> 6301e245e46eda0378eaba202f0c7bc6858edafe
 
 @NgModule({
   declarations: [
@@ -37,28 +37,21 @@ import {WebcamModule} from "ngx-webcam";
     HexagonHoverComponent,
     NeomorphComponent,
     DropdownMenuComponent,
-<<<<<<< HEAD
     CustomCompsComponent,
     PanelComponent,
-    SlideComponent
+    SlideComponent,
+    TempComponent,
+    GlassMorphismComponent,
+    ComponentsComponent,
+    SideMenuComponent,
+    VideoComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
-=======
-    GlassMorphismComponent,
-    ComponentsComponent,
-    SideMenuComponent,
-    VideoComponent
->>>>>>> 6301e245e46eda0378eaba202f0c7bc6858edafe
+    AppRoutingModule,
+    WebcamModule
   ],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        AppRoutingModule,
-        WebcamModule
-    ],
   providers: [],
   bootstrap: [AppComponent]
 })
