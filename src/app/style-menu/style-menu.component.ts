5bba0da4fac90c2e3877e83ff268cc29e5182a28
KarmitP98 <karmit199@gmail.com>
2022-02-13 13:30:53 -0500

    update(master): Attempt to make moving background menu work.

--- src/app/style-menu/style-menu.component.ts
+++ src/app/style-menu/style-menu.component.ts
@@ -15,4 +15,10 @@ export class StyleMenuComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  selectMenu = (link: string, event: any) => {
+    this.currentMenu = link;
+    const left = event.currentTarget.getBoundingClientRect().left;
+    const activeButton = document.querySelector('.active-link');
+  };
+
 }
